36eecf1d4f2f86afba2e89559284305a8c249f35
addy360 <abbyboy360@gmail.com>
2021-11-29 17:51:00 +0300

    link spans fade

--- components/Navigation/NavItem.js
+++ components/Navigation/NavItem.js
@@ -6,7 +6,9 @@ const NavItem = ({ icon, href, name }) => {
       <Link href={href}>
         <a className="flex items-center w-full p-4 hover:text-primary-50 hover:bg-primary-50 hover:bg-opacity-30 cursor-pointer transition-colors ">
           <span className="mx-5">{icon}</span>
-          <span className="ml-2 text-sm whitespace-nowrap">{name}</span>
+          <span className="ml-2 text-sm whitespace-nowrap group-hover:opacity-100 opacity-0 transition-all ">
+            {name}
+          </span>
         </a>
       </Link>
     </li>
