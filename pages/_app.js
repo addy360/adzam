95fb1164bd1205c5ba23c941557cdbbfb3eb94c9
addy360 <abbyboy360@gmail.com>
2021-11-29 14:43:11 +0300

    cleanup!!

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import 'tailwindcss/tailwind.css'
+import "tailwindcss/tailwind.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="font-ubuntu">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,31 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ubuntu: "Ubuntu",
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#050F1E",
+          50: "#1F5EBB",
+          100: "#1C55AA",
+          200: "#164387",
+          300: "#113264",
+          400: "#0B2041",
+          500: "#050F1E",
+          600: "#000000",
+          700: "#000000",
+          800: "#000000",
+          900: "#000000",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
