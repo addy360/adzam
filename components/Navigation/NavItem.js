c0e5cb6c99bc8ccca35bf8a15dc5af0387febebe
addy360 <abbyboy360@gmail.com>
2021-11-30 11:17:01 +0300

    active link

--- components/Navigation/NavItem.js
+++ components/Navigation/NavItem.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 const NavItem = ({ icon, href, name }) => {
+  const { pathname } = useRouter();
+
+  const activeClasses =
+    pathname === href && `text-primary-50 bg-primary-50 bg-opacity-20`;
+
   return (
     <li className="block">
       <Link href={href}>
-        <a className="flex items-center w-full p-4 hover:text-primary-50 hover:bg-primary-50 hover:bg-opacity-30 cursor-pointer transition-colors ">
+        <a
+          className={`flex items-center w-full p-4 hover:text-primary-50 hover:bg-primary-50 hover:bg-opacity-10 cursor-pointer transition-colors ${activeClasses} `}
+        >
           <span className="mx-5">{icon}</span>
           <span className="ml-2 text-sm whitespace-nowrap group-hover:opacity-100 opacity-0 transition-all ">
             {name}
