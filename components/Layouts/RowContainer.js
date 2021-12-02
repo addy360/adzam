da5958987a925b14cab0dbcfcb3d90e71b2e6dae
addy360 <abbyboy360@gmail.com>
2021-12-02 15:11:24 +0300

    responsive images

--- components/Layouts/RowContainer.js
+++ components/Layouts/RowContainer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const RowContainerItem = () => {
   return (
-    <div className="w-[200px] h-[100px] cursor-pointer shadow-md hover:shadow-2xl hover:scale-110 transition-all flex-shrink-0  overflow-hidden rounded-md">
+    <div className="w-[150px] md:w-[200px] h-[80px] md:h-[100px] cursor-pointer shadow-md hover:shadow-2xl hover:scale-110 transition-all flex-shrink-0  overflow-hidden rounded-md">
       <img
         src="https://source.unsplash.com/random"
         className="h-full w-full object-cover object-center"
@@ -15,7 +15,7 @@ const RowContainerItem = () => {
 const RowContainer = ({ title }) => {
   return (
     <section className="my-5  w-full p-5  ">
-      <h1 className="font-light text-2xl">{title}</h1>
+      <h1 className="font-light md:text-2xl">{title}</h1>
       <div className="overflow-x-auto w-full p-5  flex gap-10">
         <RowContainerItem />
         <RowContainerItem />
