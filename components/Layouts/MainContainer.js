0b7b812be34387075c09667fde4f0abe6c21fdf5
addy360 <abbyboy360@gmail.com>
2021-12-02 15:20:05 +0300

    logo mods

--- components/Layouts/MainContainer.js
+++ components/Layouts/MainContainer.js
@@ -9,7 +9,9 @@ const MainContainer = ({ children, title }) => {
       </Head>
       <div className="bg-primary-500 text-white h-screen w-screen pt-5 overflow-y-auto overflow-x-hidden ">
         <div className=" h-full w-[88px] group  hover:w-[243px] fixed  top-0 bottom-0 left-0 overflow-hidden transition-all bg-primary z-20 ">
-          <h1 className="text-2xl uppercase text-center mb-5">Adzam</h1>
+          <h1 className="md:text-2xl font-serif font-semibold animate-pulse uppercase text-center my-5 ">
+            Adzam
+          </h1>
           <Nav />
           <div className=" flex flex-col gap-3 mt-5  px-5 text-sm ">
             <button className="mx-auto inline-block w-full whitespace-nowrap px-3 py-1 text-center border border-transparent group-hover:border-white transition-all rounded-md ">
