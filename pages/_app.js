956c7913e9d37a977c6a8cbea307768f1f00ef6d
addy360 <abbyboy360@gmail.com>
2022-01-06 18:31:27 +0300

    meta

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,43 @@ function MyApp({ Component, pageProps }) {
   return (
     <div className="font-ubuntu">
       <Head>
+        <meta
+          name="description"
+          content="Azam max website clone build out of admiration for the site."
+        />
+        <meta
+          property="og:description"
+          content="Azam max website clone build out of admiration for the site."
+        />
+        <meta
+          name="twitter:description"
+          content="Azam max website clone build out of admiration for the site."
+        />
+        <meta
+          property="og:image"
+          content="https://avatars.githubusercontent.com/u/51880591?v=4?s=400"
+        />
+
+        <meta
+          name="twitter:image:src"
+          content="https://avatars.githubusercontent.com/u/51880591?v=4?s=400"
+        />
+        <meta name="twitter:site" content="@adzam" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="adzam" />
+
+        <meta
+          property="og:image"
+          content="https://avatars.githubusercontent.com/u/51880591?v=4?s=400"
+        />
+        <meta
+          property="og:image:alt"
+          content="Azam max website clone build out of admiration for the site."
+        />
+        <meta property="og:site_name" content="adzam" />
+        <meta property="og:type" content="profile" />
+        <meta property="og:title" content="adzam " />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
